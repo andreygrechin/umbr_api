e847195931d3838076cc7ab372477222b4349892
kolatz <36399892+kolatz@users.noreply.github.com>
2019-05-06 00:35:41 +0300

    bump version

--- umbr_api/__about__.py
+++ umbr_api/__about__.py
@@ -16,10 +16,10 @@ __title__ = "umbr_api"
 __summary__ = "Cisco Umbrella APIs wrapper and command-line utility"
 __uri__ = "https://github.com/kolatz/umbr_api"
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 __author__ = "kolatz"
 __email__ = "private@example.com"
 
 __license__ = "MIT"
-__copyright__ = "Copyright 2018 %s" % __author__
+__copyright__ = "Copyright 2019 %s" % __author__
